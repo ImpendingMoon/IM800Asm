cf07066060dca43dabac3d63cee1a76464017b77
ImpendingMoon <theimpendingmoon@gmail.com>
2026-07-01 20:30:10 -0500

    Implement parser

--- Parser.cs
+++ Parser.cs
@@ -51,11 +51,27 @@ internal class Parser
 		{
 			result.ResultObject = new EndOfFileStatement(t.Line, t.Column);
 		}
+		else if (token is SymbolToken nl && nl.Type == Constants.TokenType.NewLine)
+		{
+			Advance();
+		}
 		else if (TryParseLabel(out Result<LabelStatement?> labelResult))
 		{
 			Debug.Assert(labelResult.ResultObject is not null);
 			result.Combine(labelResult);
 			result.ResultObject = labelResult.ResultObject;
+		}
+		else if (TryParseDirective(out Result<DirectiveStatement?> directiveResult))
+		{
+			Debug.Assert(directiveResult.ResultObject is not null);
+			result.Combine(directiveResult);
+			result.ResultObject = directiveResult.ResultObject;
+		}
+		else if (TryParseInstruction(out Result<InstructionStatement?> instructionResult))
+		{
+			Debug.Assert(instructionResult.ResultObject is not null);
+			result.Combine(instructionResult);
+			result.ResultObject = instructionResult.ResultObject;
 		}
 		else
 		{
@@ -74,6 +90,7 @@ internal class Parser
 		{
 			result.ResultObject = new(t.Line, t.Column, t.Lexeme);
 			Advance(2);
+			return true;
 		}
 
 		return false;
@@ -85,15 +102,39 @@ internal class Parser
 
 		if (Current() is IdentifierToken t)
 		{
-			// TODO: strip size suffix (ends with /\../ )
-			// TODO: tryparse instruction
-			// TODO: parseoperands
+			string canon = t.Lexeme.ToUpperInvariant();
+			Constants.Size? size = null;
+
+			if (canon.Length < 2)
+			{
+				return false;
+			}
+
+			// A size suffix is exactly one character after the separator, e.g. "LD.B".
+			if (canon[^2] == '.' && TryParseInstructionSize(canon[^1], out Constants.Size? parsedSize))
+			{
+				size = parsedSize;
+				canon = canon[..^2];
+			}
+
+			if (Enum.TryParse(canon, ignoreCase: true, out Constants.Instruction instruction))
+			{
+				InstructionStatement statement = new(t.Line, t.Column, instruction, size);
+				Advance();
+
+				Result<List<Operand>> operandsResult = ParseOperands();
+				result.Combine(operandsResult);
+				statement.Operands = operandsResult.ResultObject;
+
+				result.ResultObject = statement;
+				return true;
+			}
 		}
 
 		return false;
 	}
 
-	private bool TryParseInstructionSize(char suffix, out Constants.Size? size)
+	private static bool TryParseInstructionSize(char suffix, out Constants.Size? size)
 	{
 		switch (char.ToUpper(suffix))
 		{
@@ -118,9 +159,26 @@ internal class Parser
 
 		if (Current() is IdentifierToken t)
 		{
-			// TODO strip optional leading period
-			// TODO tryparse directive
-			// TODO parseoperands
+			string mnemonic = t.Lexeme;
+
+			// Directives may have a leading period, e.g. ".ORG".
+			if (mnemonic.StartsWith('.'))
+			{
+				mnemonic = mnemonic[1..];
+			}
+
+			if (Enum.TryParse(mnemonic, ignoreCase: true, out Constants.Directive directive))
+			{
+				DirectiveStatement statement = new(t.Line, t.Column, directive);
+				Advance();
+
+				Result<List<Operand>> operandsResult = ParseOperands();
+				result.Combine(operandsResult);
+				statement.Operands = operandsResult.ResultObject;
+
+				result.ResultObject = statement;
+				return true;
+			}
 		}
 
 		return false;
@@ -150,6 +208,12 @@ internal class Parser
 					operands.Add(memoryOperandResult.ResultObject);
 				}
 			}
+			else if (c is StringToken st)
+			{
+				// A string decomposes into a list of byte literals
+				operands.AddRange(ParseStringOperand(st));
+				Advance();
+			}
 			else
 			{
 				Result<Operand?> operandResult = ParseOperand();
@@ -161,13 +225,12 @@ internal class Parser
 				}
 			}
 
-			if (IsEndOfOperand(c))
+			c = Current();
+
+			if (c is SymbolToken ct && ct.Type == Constants.TokenType.Comma)
 			{
 				// Allow ending a line on a comma to continue list on new line
-				if (
-					c is SymbolToken ct && ct.Type == Constants.TokenType.Comma
-					&& Next() is SymbolToken n && n.Type == Constants.TokenType.NewLine
-				)
+				if (Next() is SymbolToken n && n.Type == Constants.TokenType.NewLine)
 				{
 					Advance(2);
 				}
@@ -176,13 +239,27 @@ internal class Parser
 					Advance();
 				}
 			}
-			else
+			else if (!IsEndOfOperand(c))
 			{
 				result.AddError("Parser", $"{c.Line}:{c.Column}:\texpected end of operand");
+				Advance();
 			}
 		}
 
 		return result;
+	}
+
+	private static List<Operand> ParseStringOperand(StringToken token)
+	{
+		List<Operand> operands = [];
+
+		foreach (byte b in token.StringData)
+		{
+			NumberToken byteToken = new(token.Line, token.Column, token.Lexeme, b);
+			operands.Add(new ExpressionOperand(token.Line, token.Column, [byteToken]));
+		}
+
+		return operands;
 	}
 
 	private Result<Operand?> ParseOperand()
@@ -234,11 +311,49 @@ internal class Parser
 	{
 		Result<Operand?> result = new(null);
 
-		Token c = Current();
+		Token start = Current(); // '['
+		Advance();
 
-		// TODO: Handle indirect register/expression, indexed
+		Token inner = Current();
 
-		// TODO: Expect and consume RBracket before end of operand
+		if (inner is IdentifierToken idt && Enum.TryParse(idt.Lexeme, ignoreCase: true, out Constants.Register register))
+		{
+			Advance();
+
+			if (Current() is SymbolToken sign && sign.Type is Constants.TokenType.Plus or Constants.TokenType.Minus)
+			{
+				// indexed
+				List<Token> tokens = ParseExpression();
+
+				result.ResultObject = new IndexedOperand(start.Line, start.Column, register, tokens);
+			}
+			else
+			{
+				// indirect
+				result.ResultObject = new IndirectRegisterOperand(start.Line, start.Column, register);
+			}
+		}
+		else if (IsExpressionStart(inner))
+		{
+			// direct
+			List<Token> tokens = ParseExpression();
+			result.ResultObject = new IndirectExpressionOperand(start.Line, start.Column, tokens);
+		}
+		else
+		{
+			result.AddError("Parser", $"{inner.Line}:{inner.Column}:\texpected register or expression");
+			Advance();
+		}
+
+		if (Current() is SymbolToken rb && rb.Type == Constants.TokenType.RBracket)
+		{
+			Advance();
+		}
+		else
+		{
+			Token c = Current();
+			result.AddError("Parser", $"{c.Line}:{c.Column}:\texpected ']'");
+		}
 
 		return result;
 	}
@@ -315,12 +430,58 @@ internal class Parser
 	{
 		result = new(null);
 
-		if (Current() is IdentifierToken t)
+		if (!IsExpressionStart(Current()))
 		{
-			// TODO recursive descent parser in new ExpresisonParser class
+			return false;
 		}
 
-		return false;
+		Token start = Current();
+		List<Token> tokens = ParseExpression();
+		result.ResultObject = new ExpressionOperand(start.Line, start.Column, tokens);
+
+		return true;
+	}
+
+	private List<Token> ParseExpression()
+	{
+		// Gather all tokens in this operand, recursive descent parser will be later
+		List<Token> tokens = [];
+		Token t = Current();
+
+		while (!IsEndOfExpression(t))
+		{
+			tokens.Add(t);
+			Advance();
+			t = Current();
+		}
+
+		return tokens;
+	}
+	private static bool IsExpressionStart(Token token)
+	{
+		if (token is NumberToken or IdentifierToken)
+		{
+			return true;
+		}
+
+		return token is SymbolToken t
+			&& t.Type
+			is Constants.TokenType.Plus
+			or Constants.TokenType.Minus
+			or Constants.TokenType.Tilde
+			or Constants.TokenType.Exclamation
+			or Constants.TokenType.Dollar
+			or Constants.TokenType.LParen;
+	}
+
+	private static bool IsEndOfExpression(Token token)
+	{
+		return token is SymbolToken t
+			&& t.Type
+			is Constants.TokenType.Comma
+			or Constants.TokenType.NewLine
+			or Constants.TokenType.EndOfFile
+			or Constants.TokenType.RBracket;
 	}
 
 	private static bool IsEndOfOperand(Token token)
